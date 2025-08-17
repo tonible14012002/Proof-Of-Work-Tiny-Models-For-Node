d651301033a988e3bb0229d7566205adc2480bc1
tonible14012002 <namanh14.work@gmail.com>
2025-08-17 15:48:44 +0700

    feat: init pj

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div className='bg-amber-300'>
+      aoejwfoaj
+
+    </div>
+  </StrictMode>,
+)
